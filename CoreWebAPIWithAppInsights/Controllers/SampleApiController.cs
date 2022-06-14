8eb46ce50605663e35740b8ceed1cbfcf03215af
nirzaf <mfmfazrin1986@gmail.com>
2022-06-14 23:19:35 +0530

    using Microsoft.AspNetCore.Hosting; using Microsoft.Extensions.Configuration; using Microsoft.Extensions.DependencyInjection;

--- CoreWebAPIWithAppInsights/Controllers/SampleApiController.cs
+++ CoreWebAPIWithAppInsights/Controllers/SampleApiController.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreWebAPIWithAppInsights.Controllers
